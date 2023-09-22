b8680757eeba1312692c0b76e3fb8f539a30db44
PascalSeth <pascalelikem@gmail.com>
2023-09-22 12:59:34 +0100

    new layput

--- src/components/Busbooking.js
+++ src/components/Busbooking.js
@@ -28,9 +28,33 @@ function Busbooking() {
     [23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43],
     [24, 26, 28, 30, 32, 34, 36, 38, 40, 42,44 ],
   ];
+  // Generate an array of seat numbers (1-45)
+  const seatCNumbers = Array.from({ length: 45 }, (_, index) => index + 1);
+
+  // Initialize a state to keep track of selected seats
+  const [selectedCseats, setSelectedCseats] = useState([]);
+
+  // Function to handle seat selection
+  const toggleSeatCSelection = (seatCNumber) => {
+    if (selectedCseats.includes(seatCNumber)) {
+      setSelectedCseats(selectedCseats.filter((seat) => seat !== seatCNumber));
+    } else {
+      setSelectedCseats([...selectedCseats, seatCNumber]);
+    }
+  };
+
+  // Split the seat numbers into two ranges
+  const firstRange = seatCNumbers.slice(0, 11);
+  const secondRange = seatCNumbers.slice(11,22);
+  const thirdRange = seatCNumbers.slice(22,33);
+  const fourthRange = seatCNumbers.slice(33,44);
+
+
+
 
   return (
     <div className='bg-white p-[2vw] flex flex-col items-center'>
+      <div className='max-lg:hidden'>
       <div className='mb-[5vh] flex-col '>  
           {seatGrid.map((row, rowIndex) => (
         <div key={rowIndex} className='flex'>
@@ -73,8 +97,82 @@ function Busbooking() {
           ))}
         </div>
       ))}
+      </div></div>
+
+      <div className='flex lg:hidden '>
+        <div className='flex'>
+      <div>
+        {firstRange.map((seatCNumber) => (
+          <div
+            key={seatCNumber}
+            onClick={() => toggleSeatCSelection(seatCNumber)}
+            className='seatings flex-col mx-[1vw] max-lg:mx-[2vw] my-[1vh] w-fit cursor-pointer
+            border-gray-400 
+            items-center flex border rounded-lg maxl'
+          >
+            <AirlineSeatReclineNormalOutlinedIcon />
+            {seatCNumber}
+          </div>
+        ))}
       </div>
-  
+      <div>
+        {secondRange.map((seatCNumber) => (
+          <div
+            key={seatCNumber}
+            onClick={() => toggleSeatCSelection(seatCNumber)}
+            className='seatings flex-col mx-[1vw] max-lg:mx-[2vw] my-[1vh] w-fit cursor-pointer
+            border-gray-400 
+            items-center flex border rounded-lg maxl'
+          >
+            <AirlineSeatReclineNormalOutlinedIcon />
+            {seatCNumber}
+          </div>
+        ))}
+      </div>
+      
+      
+      </div>
+
+      <div>
+</div>
+<div className='flex'>
+<div className='flex ml-[6vw]'>
+      <div>
+        {thirdRange.map((seatCNumber) => (
+          <div
+            key={seatCNumber}
+            onClick={() => toggleSeatCSelection(seatCNumber)}
+            className='seatings flex-col mx-[1vw] max-lg:mx-[2vw] my-[1vh] w-fit cursor-pointer
+            border-gray-400 
+            items-center flex border rounded-lg maxl'
+          >
+            <AirlineSeatReclineNormalOutlinedIcon />
+            {seatCNumber}
+          </div>
+        ))}
+      </div>
+      <div>
+        {fourthRange.map((seatCNumber) => (
+          <div
+            key={seatCNumber}
+            onClick={() => toggleSeatCSelection(seatCNumber)}
+            className='seatings flex-col mx-[1vw] max-lg:mx-[2vw] my-[1vh] w-fit cursor-pointer
+            border-gray-400 
+            items-center flex border rounded-lg maxl'
+          >
+            <AirlineSeatReclineNormalOutlinedIcon />
+            {seatCNumber}
+          </div>
+        ))}
+      </div>
+      
+      
+      </div>
+</div>
+      </div>
+
+
+
      
     </div>
   );
