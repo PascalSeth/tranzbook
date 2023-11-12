3fd1a29cf6bd73679310a5df896f29852209208c
PascalSeth <pascalelikem@gmail.com>
2023-11-12 08:53:59 +0000

    servw

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -23,7 +23,7 @@ axios.defaults.withCredentials = true;
   const handleSubmit = async (event) => {
     event.preventDefault();
     try{
-      const url ="https://server-mauve-iota.vercel.app/auth"
+      const url ="https://server-ashy-six-37.vercel.app/auth"
       const {data:res} = await axios.post(url,data)
             localStorage.setItem("token",res.data)
 navigate("/")
